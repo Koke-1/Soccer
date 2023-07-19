02bfb18e23da8e584179fe86b9eae82c969a842f
Koke <sanjuangeorge@gmail.com>
2023-07-19 12:57:20 -0500

    update base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/SoccerGame/`,
+  base: `/Soccer/`,
   plugins: [react()],
   build:{
     assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.ico', '**/*.glb']
